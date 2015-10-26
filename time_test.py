55585cdf03fe52bc4af32d279e5e9be144015bbb
Salvador Gaviota <tomgranuja@gmail.com>
2015-10-26 14:10:31 -0300

    time_test simula mouseRelease en fscreengame
    Se incluye y extiende fscreengame.FullBox() en time_test para lanzar
    un sliderMouseRelease a 5 y 10 segundos de haber sido lanzada la
    aplicación en fullscreen.

--- time_test.py
+++ time_test.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3 -tt
 #-*- coding:utf-8 -*-
 
-import time
+import sys, time
 from threading import Timer
+import fscreengame
 
+class Form(fscreengame.FullBox):
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        self.whiteBox.setTimers(fbtime      = 1000,
+                        blinktime   =  500,
+                        blinkperiod =  100)
+    
+    def showFullScreen(self):
+        super(Form,self).showFullScreen()
+        run_at_some_times()
+    
 def print_and_launch():
     print("Launched at", time.time())
     f()
@@ -11,10 +23,12 @@ def run_at_some_times():
      print('Test start time', time.time())
      Timer(5, print_and_launch, ()).start()
      Timer(10, print_and_launch, ()).start()
-     time.sleep(11)
-     print('Test stoped at', time.time())
 
 def f():
-    pass
-    
-run_at_some_times()
+    rate = 0.325
+    form.whiteBox.slider.sliderMouseRelease.emit(rate)
+
+app  = fscreengame.QApplication(sys.argv)
+form = Form()
+form.showFullScreen()
+sys.exit(app.exec_())
