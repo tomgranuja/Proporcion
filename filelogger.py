b629d26807d0c1fcdda305c938ee6e4e98ffd60f
Salvador Gaviota <tomgranuja@gmail.com>
2015-11-10 16:32:39 -0300

    Agrega modulo de escritura en archivo
    Separa la clase Logger() en el módulo filelogger.py. Las instancias
    poseen una ruta de archivo, y el método write(*args), escribe todos
    los argumentos pasados separados por espacio en una misma línea. En
    inicialización, reescribe el archivo dejando su nombre en la línea de
    cabecera. Dos funciones de conveniencia arman el nombre de archivo a
    partir de una UID, y número y tipo de sesión opcionales:
      -practiceLogPath()
      -testLogpath()

---
+++ filelogger.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3 -tt
+#-*- coding:utf-8 -*-
+
+def testLogPath(uid, sess = 1, stype = 'T'):
+    logDir = ''
+    fpath = '{}{}_S{:02}_{}'.format(logDir, uid, sess, stype)
+    return fpath
+
+def practiceLogPath(uid, sess = 1, stype = 'T'):
+    logDir = ''
+    fpath = '{}{}_P{:02}_{}'.format(logDir, uid, sess, stype)
+    return fpath
+    
+
+class Logger():
+    def __init__(self, fpath):
+        self.fpath = fpath
+        self.initFile()
+        
+    def initFile(self):
+        with open(self.fpath, 'w') as f:
+            head = '{}\n'.format(self.fpath)
+            f.write(head)
+        
+    def write(self, *args):
+        with open(self.fpath, 'a')as f:
+            arg_strings = [ str(arg) for arg in args ]
+            line = ' '.join(arg_strings) + '\n'
+            f.write(line)
+
+if __name__ == "__main__":
+    practPath = practiceLogPath('ABC')
+    practLogger = Logger(practPath)
+    n, t, r = (0, 3000, 0.3)
+    practLogger.write(n, t, r)
+    
+    testPath  = testLogPath('ABC')
+    testLogger  = Logger(testPath)
+    n, t, r = (0,2500, 0.75)
+    testLogger.write(n, t, r)
+    
