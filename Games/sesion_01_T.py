7fe3c4fb404a76d6e537325e03497cf922d4acb9
Salvador Gaviota <tomgranuja@gmail.com>
2015-11-13 15:29:47 -0300

    Dir Games con lanzador de primera sesion
    Se intenta exponer solo el script lanzador del juego en un directorio
    limpio. El lanzador sólo emula el llamado de fscreengame
    importándolo.

---
+++ Games/sesion_01_T.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3 -tt
+#-*- coding:utf-8 -*-
+
+APP_DIR = '..'
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+sys.path.append(APP_DIR)
+import fscreengame
+
+if __name__ == "__main__":
+    app  = QApplication(sys.argv)
+    form = fscreengame.FullBox()
+    if form.userUid == None:
+        print("No se identificó usuario, saliendo.")
+        sys.exit(QDialog.Rejected)
+    form.showFullScreen()
+    sys.exit(app.exec_())
