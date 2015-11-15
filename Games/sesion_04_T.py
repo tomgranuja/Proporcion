80d7959d0c12bc61440b9ebaa37a49549cc7fed0
Salvador Gaviota <tomgranuja@gmail.com>
2015-11-15 07:47:16 -0300

    Agrega imágenes y color de sesiones 2-4

--- Games/sesion_04_T.py
+++ Games/sesion_04_T.py
@@ -13,9 +13,9 @@ import fscreengame, inputdata
 SESSION          = 4
 CONTROL          = False
 TEST_ERRORS      = (0.15, 0.05) ##Tamaño del feedback
-INTRO_PIXMAP     = 'intro_sesion1_630.png' ##Imagen de intro
-SLIDER_PIXMAPS   = ['i_sesion1.png','d_sesion1.png'] ##Extremos
-FRUIT_BAR_RGB    = (254,0,0)   ##Color rgb de la barra
+INTRO_PIXMAP     = 'intro_sesion4_630.png' ##Imagen de intro
+SLIDER_PIXMAPS   = ['i_sesion4.png','d_sesion4.png'] ##Extremos
+FRUIT_BAR_RGB    = (255,192,0)   ##Color rgb de la barra
 EXP_STR         = inputdata.t_sesion04 ##Estímulos de experimento
 PRACTICE_STR     = ''
 #PRACTICE_STR     = inputdata.tPractice
